82d4279044485f8f8eaa018f7d7c2820eec0c1df
xmasuhai <xmasuhai@163.com>
2021-06-16 16:50:40 +0800

    echarts 移动端适配

--- src/modules/fitScreen.js
+++ src/modules/fitScreen.js
@@ -1,4 +1,10 @@
+// 当前视口宽度
 const myScreenWidth = document.documentElement.clientWidth
+
+// 换算方法
+function nowSize(originalFontSizePX, initWidth = 1920) {
+  return originalFontSizePX * (myScreenWidth / initWidth)
+}
 
 export default function (chartDom) {
   if (!chartDom) {return}
